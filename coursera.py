c0bca52317bb6b7d53ebb34dfe0a0d35bf653707
Kartik <hi@kartikt.com>
2012-08-17 04:26:12 +0000

    Functioning Content Grabber Added

---
+++ coursera.py
@@ -0,0 +1,84 @@
+import re
+import sys
+import os
+import mechanize
+import BeautifulSoup
+
+
+
+class Coursera:
+
+    def __init__(self, options):
+        self.username = options["user"]
+        self.password = options["pass"]
+        self.course   = options["course"]
+
+        self.browser  = mechanize.Browser()
+
+
+    def login(self, course=None):
+        if course is None:
+            course = self.course
+
+        loginurl  = "https://www.coursera.org/" + course + "/auth/auth_redirector?"
+        loginurl += "type=login&subtype=normal&email="
+        homeurl   = "https://class.coursera.org/" + course + "/class/index"
+
+        self.browser.open(loginurl)
+        self.browser.select_form(nr=0)
+        self.browser.form["email"]    = self.username
+        self.browser.form["password"] = self.password
+        self.browser.submit()
+
+        main   = self.browser.open(homeurl).read()
+        search = re.findall('dropdown_my', main)
+
+        if len(search) == 1:
+            return False
+
+        return True
+
+
+    def getContent(self, pageurl=None):
+        if pageurl is None:
+            pageurl = "https://class.coursera.org/" + self.course + "/lecture/index"
+
+        data = self.browser.open(pageurl).read()
+        html = BeautifulSoup.BeautifulSoup(data)
+        resp = {}
+
+        for week in html.findAll('h3', {'class':'list_header'}):
+            topic = week.string
+            temp  = {}
+
+            for lectures in week.parent.nextSibling.findAll('li'):
+                lecture = self._strip(lectures.a.contents[0])
+                lecurls = []
+               
+                for links in lectures.findAll('a'):
+                    lecurls.append(links['href'])
+
+                temp[lecture] = lecurls
+
+            resp[topic] = temp
+
+
+        return resp
+
+
+
+    def _strip(self, data):
+        return re.sub('(\n|\r|\t)', '', data)
+
+
+from pprint import pprint as pp
+
+USERNAME = ''
+PASSWORD = ''
+
+options = {"user": USERNAME, "pass": PASSWORD, "course": "nlp"}
+course = Coursera(options)
+course.login()
+pp(course.getContent())
+
+
